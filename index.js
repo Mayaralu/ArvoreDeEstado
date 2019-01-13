ab7fd9fe9f18ad4331019d4ca6c3a3fbf8d7af8d
Mayaralu <mayara.lu.98@gmail.com>
2019-01-13 21:31:38 -0200

    Create index.js
    Criação do Armazenador

---
+++ index.js
@@ -0,0 +1,12 @@
+function createStore() {
+	// The store should have four parts
+	// 1. The State
+	// 2. Get the state
+	// 3. Listen to changes on the state
+	// 4. Update the state
+
+	let state // variável que armazena o estado
+
+	const getState = () => state // função que retorna o estado
+	
+}
