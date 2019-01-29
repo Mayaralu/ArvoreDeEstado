9052688b217ca34b52a5f4c81e63eef2a461f9b7
Mayaralu <mayara.lu.98@gmail.com>
2019-01-28 22:58:33 -0200

    Melhorando o código

--- index.js
+++ index.js
@@ -37,6 +37,48 @@ function app (state = {}, action){
 	}
 }
 
+// App Code
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function addTodoAction (todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  }
+}
+
+function removeTodoAction (id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  }
+}
+
+function toggleTodoAction (id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  }
+}
+
+function addGoalAction (goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
+}
+
+function removeGoalAction (id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
+}
+
 // Library Code
 function createStore(reducer) {
 	// The store should have four parts
